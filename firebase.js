617602aa497a5cf767586cdf3f5ca6500b9255f7
vinnTonae <alvinkarms5511@gmail.com>
2026-06-22 03:17:27 +0300

    Firebase Render configuration

--- firebase.js
+++ firebase.js
@@ -1,10 +1,18 @@
-const { initializeApp, cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = require('./firebase-key.json');
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(
+    /\\n/g,
+    "\n"
+  ),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 const app = initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert(serviceAccount),
 });
 
 const db = getFirestore(app);
